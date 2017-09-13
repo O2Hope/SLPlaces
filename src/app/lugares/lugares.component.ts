0a0a48649d19859d1d8243be6f481e29af664fba
Agustin Ruiz <agusvaldes2@live.com>
2017-09-12 21:33:42 -0500

    QueryParams
    Pasar parametros por query

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -9,9 +9,9 @@ export class LugaresComponent implements OnInit {
 
   title = 'SLPlaces';
   lugares: any = [
-    {plan: 'pagado', cercania: 1, distancia: 1, nombre: 'pasteleria', active: true},
-    {plan: 'gratis', cercania: 2, distancia: 5, nombre: 'floreria', active: true},
-    {plan: 'gratis', cercania: 3, distancia: 10, nombre: 'veterinaria', active: false}
+    {id: 1, plan: 'pagado', cercania: 1, distancia: 1, nombre: 'pasteleria', active: true},
+    {id: 2, plan: 'gratis', cercania: 2, distancia: 5, nombre: 'floreria', active: true},
+    {id: 3, plan: 'gratis', cercania: 3, distancia: 10, nombre: 'veterinaria', active: false}
   ];
   lat = 22.1473734;
   lng = -101.0106106;
