825f110e128feff6118bfb60aaf082b5838dde60
Agustin Ruiz <agusvaldes2@live.com>
2017-09-12 22:31:53 -0500

    Vista Detalles
    Se agrego la vista de detalles y se utilizo el metodo find()

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -8,11 +8,25 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class DetalleComponent implements OnInit {
 
+  lugares: any = [
+    {id: 1, plan: 'pagado', cercania: 1, distancia: 1, nombre: 'pasteleria', active: true, detalle: 'Actualizando detalles del lugar'},
+    {id: 2, plan: 'gratis', cercania: 2, distancia: 5, nombre: 'floreria', active: true, detalle: 'Actualizando detalles del lugar'},
+    {id: 3, plan: 'gratis', cercania: 3, distancia: 10, nombre: 'veterinaria', active: false, detalle: 'Actualizando detalles del lugar'}
+  ];
+  id: number = null;
+  lugar: any = {};
+
   constructor(private route: ActivatedRoute) {
 
     console.log(this.route.snapshot.params['id']);
-    console.log(this.route.queryParams);
+    console.log(this.route.snapshot.queryParams['action']);
+    this.id = this.route.snapshot.params['id'];
+    this.lugar = this.buscarId();
 
+  }
+
+  buscarId() {
+      return this.lugares.find((l) => l.id == this.id);
   }
 
   ngOnInit() {
